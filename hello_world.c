6e38b1b3c4873311d594c4e74b33454faa7fcb30
Sadie <158243586+simeks18@users.noreply.github.com>
2026-02-26 11:37:11 -0500

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+void main () {
+
+printf("Hello Class\n");
+printf("Today is tuesday\n");
+
+}
---
+++ number.c
@@ -0,0 +1,15 @@
+
+// in class system programming on 1.27.26
+
+
+#include <stdio.h>
+
+int main (){
+
+float num = 2.22;
+long number = 2.2222222222222222;
+printf("My number is: %f \n", num);
+printf("My new number is: %lld", number);
+
+return 0;
+}
